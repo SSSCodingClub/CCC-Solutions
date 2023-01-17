0efc15697c7010843a510a8594894791c0d9ef8c
Skullheadx <704277@pdsb.net>
2023-01-17 16:08:02 -0500

    Update Problem J3 Art (2).py

--- 2020/Junior/Problem J3 Art (2).py
+++ 2020/Junior/Problem J3 Art (2).py
@@ -2,13 +2,12 @@ drops = int(input())
 left, right, top, bottom = 100, 0, 0, 100
 
 for i in range(drops):
-    point = input().split(",")
-    x, y = int(point[0]), int(point[1])
+    x, y = tuple(map(int, input().split(",")))
 
     left = min(left, x)
     right = max(right, x)
     bottom = min(bottom, y)
     top = max(top, y)
 
-print(f"{left - 1}, {bottom - 1}")
-print(f"{right + 1}, {top + 1}")
+print(left - 1, bottom - 1, sep=",")
+print(right + 1, top + 1, sep=",")
