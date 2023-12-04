7bf43c8db85aeb01716f86086137bae259b8e066
Skullheadx <94652084+Skullheadx@users.noreply.github.com>
2023-12-03 20:32:14 -0500

    Update Problem S2 Symmetric Mountains.py

--- 2023/Senior/Problem S2 Symmetric Mountains.py
+++ 2023/Senior/Problem S2 Symmetric Mountains.py
@@ -1,28 +1,57 @@
-# Too slow for 15/15. Gets 5/15
-
-from functools import lru_cache
 N = int(input())
-heights = tuple(map(int, input().split()))
 
-@lru_cache(maxsize=None)
-def find_asymmetric_value(l, r, h=heights):
-	output = 0
-	for i in range(int((r - l) / 2) + 1):
-		output += abs(h[l + i] - h[r - i])
-	return output
+mountains = []
+
+mountains = list(map(int, input().split()))
+
+mins = []
+
+for i in range(0, N + 1):
+    mins.append(2147483647)
+
+for mid in range(0, N):
+
+        left = mid
+        right = mid;
+        size = 1;
+        assy = 0;
+        mins[size] = assy;
+
+        while (left > 0 and right < N - 1):
+
+            left -= 1
+            right += 1
+            size += 2
+
+            assy += abs(mountains[left] - mountains[right]);
+
+            mins[size] = min(assy, mins[size]);
 
 
-crops = [[] for i in range(len(heights) + 1)]
-for h in range(len(heights)):
-	l, r = h, h
-	for i in range(1, 1+ len(heights)):
-		# print(r-l + 1, heights[l:r+1])
-		if h + i < len(heights):
-			r += 1
-		crops[r - l + 1].append(find_asymmetric_value(l, r))
-		if h - i >= 0:
-			l -= 1
-		crops[r - l + 1].append(find_asymmetric_value(l, r))
-	# print()
-for i in crops[1:]:
-	print(min(i) if i else 0, end=' ')
+mid1 = 0
+mid2 = 1
+
+while(mid2 < N):
+
+        left = mid1
+        right = mid2
+
+        size = 2
+        assy = abs(mountains[mid1] - mountains[mid2])
+        mins[size] = min(assy, mins[size])
+
+        while (left > 0 and right < N - 1):
+
+            left -= 1
+            right += 1
+            size += 2
+
+            assy += abs(mountains[left] - mountains[right])
+
+            mins[size] = min(assy, mins[size])
+
+        mid1 += 1
+        mid2 += 1
+
+for i in range(1, N+1):
+        print(mins[i], end = ' ')
